b3a12a3039246f7cdb7ff67efd8e3eeadb7e62f5
Ernest Guevarra <ernest@guevarra.io>
2020-04-27 15:06:24 +0100

    update states estimator

--- stateEstimates.R
+++ stateEstimates.R
@@ -9,7 +9,7 @@ names(localityResults) <- c("stateID", "state", "localityID", "locality",
                             "Indicator", "Type", "Estimate", "LCL", "UCL", "sd")
 
 ## Locality populations for "post-stratification"
-localityPop <- sudan::population_S3M
+localityPops <- sudan::population_S3M
 
 ## Work with localities in states with all localities with populations
 subsetResults <- localityResults[localityResults$state %in% c("East Darfur",
@@ -19,71 +19,86 @@ subsetResults <- localityResults[localityResults$state %in% c("East Darfur",
                                                               "Northern",
                                                               "Sinar"), ]
 
+indicatorBase <- read.csv("indicatorBase.csv", stringsAsFactors = FALSE)
+
+allStates <- list()
+
+## Get state names
+## Organise indicator results for choropleth mapping
+xlsxFiles <- dir(path = "localityResults", pattern = "*.xlsx$")
+xlsxFiles <- xlsxFiles[xlsxFiles %in% paste("_", unique(locNames$state), 
+                                            ".xlsx", sep = "")]
+
+stateNames <- str_remove(str_remove(string = xlsxFiles, pattern = ".xlsx"), pattern = "_")
 ##
-subStates <- list()
-##
-for(i in unique(subsetResults$state)) {
+stateNames <- stateNames[stateNames %in% c("East Darfur",
+                                           "Kassala", 
+                                           "Khartoum",
+                                           "Sinar")]
+
+for(i in stateNames) {
+  ## Create concatenating list for localities in current state
+  localitiesCurrentState <- list()
+  ## Get localities for current state results file
+  localityNames <- getSheetNames(file = paste("localityResults/_", i, ".xlsx", sep = ""))
   ##
-  subLocalities <- list()
-  ##
-  x <- subset(subsetResults, state == i)
-  ##
-  for(j in unique(x$locality)) {
-    singleLocality <- subset(x, locality == j)
-    singleLocality$se <- with(singleLocality, sd / sqrt(nrow(x)))
-    subLocalities[[j]] <- subset(singleLocality, select = -c(LCL, UCL))
+  for(j in localityNames) {
+    resultsCurrentLocality <- read.xlsx(xlsxFile = paste("localityResults/_", i, ".xlsx", sep = ""),
+                                        sheet = j)
+    names(resultsCurrentLocality) <- c("Indicator", "Type", "Estimate", "LCL", "UCL", "sd")
+    resultsCurrentLocality$se <- with(resultsCurrentLocality, sd)
+    localitiesCurrentState[[j]] <- subset(resultsCurrentLocality, select = -c(LCL, UCL, sd))
   }
   ##
-  subStates[[i]] <- subLocalities
+  allStates[[i]] <- localitiesCurrentState
 }
 
-## Create workbook
+## Accumulator for pooled results
+allResults <- NULL
 resultsWB <- createWorkbook()
-## Cycle through rows (indicators)
-for(i in  names(subStates)) 
+
+## Cycle through states
+#for(i in 1:nrow(indicatorBase))
+for(i in names(allStates))
 {
-  ## Accumulator for pooled results
-  results <- NULL
-  ## Cycle through states
-  for(j in 1:nrow(subStates[[1]][[1]])) 
+  ## current state accumulator for pooled results
+  stateResults <- NULL
+  ##
+  addWorksheet(wb = resultsWB, sheetName = i)
+  ## Cycle through rows of indicators
+  for(j in 1:nrow(indicatorBase)) 
   {
-    ## Cycle through states
-    estimates <- standardErrors <- weights <- NULL    
-    ## Subset to current state
-    x <- subStates[[i]]
-    ##
-    for(k in names(x)) 
+    ## Get current state
+    currentState <- allStates[[i]]
+    ## Cycle through localities in current state
+    estimates <- standardErrors <- weights <- NULL
+    for(k in names(currentState))
     {
       ## Get estimates for current indicator, SE, and population weight in each
-      ## state
-      estimates <- c(estimates, x[[k]]$Estimate[j])
-      standardErrors <- c(standardErrors, x[[k]]$se[j])
+      ## locality in current state
+      estimates <- c(estimates, allStates[[i]][[k]]$Estimate[j])
+      standardErrors <- c(standardErrors, allStates[[i]][[k]]$se[j])
       ## Get weight for state
-      weights <- c(weights, localityPop$pop[localityPop$state == i & localityPop$locality == k])
+      weights <- c(weights, localityPops$pop[localityPops$state == i & localityPops$locality == k])
     }
-    pooledEstimate <- sum(estimates * weights, na.rm = TRUE) / sum(weights,
-                                                                   na.rm = TRUE)
-    pooledSE  <- sqrt(sum(standardErrors ^ 2 * weights / sum(weights,
-                                                           na.rm = TRUE)))
+    pooledEstimate <- sum(estimates * weights, na.rm = TRUE) / sum(weights, na.rm = TRUE)
+    pooledSE  <- sqrt(sum(standardErrors^2 * weights / sum(weights, na.rm = TRUE)))
     pooledLCL <- pooledEstimate - 1.96 * pooledSE
     pooledUCL <- pooledEstimate + 1.96 * pooledSE	
     ## Make a results row
-    resultRow <- c(subStates[[1]][[1]]$Indicator[j], pooledEstimate,
-                   pooledLCL, pooledUCL)
-    results <- rbind(results, resultRow)
+    resultRow <- c(pooledEstimate, pooledLCL, pooledUCL)
+    stateResults <- rbind(stateResults, resultRow)
   }
-  ## Convert results to a data.frame
-  results <- as.data.frame(results, stringsAsFactors = FALSE)
-  row.names(results) <- 1:nrow(results)
-  
-  names(results) <- c("Indicator", "Estimate", "LCL", "UCL")
-  for(l in 2:ncol(results))
-  {
-    results[,l] <- as.numeric(results[,l])
-  }
-  ##
-  addWorksheet(wb = resultsWB, sheetName = i)
-  writeData(wb = resultsWB, sheet = i, x = results)
-  saveWorkbook(wb = resultsWB, file = "_byStates.xlsx", overwrite = TRUE)
+  stateResults <- data.frame(indicatorBase[ , 1], stateResults)
+  names(stateResults) <- c("Indicator", "Estimator", "LCL", "UCL")
+  writeData(wb = resultsWB, sheet = i, x = stateResults)
+  allResults <- rbind(allResults, stateResults)
 }
+
+saveWorkbook(wb = resultsWB, file = "_byStates.xlsx", overwrite = TRUE)
+
+
+
+
+
 
